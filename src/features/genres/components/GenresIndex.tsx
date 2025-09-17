b8d56ccd89efeff86539349cae0e2205a1a7abb5
AccountsStatements <edward_el_doc@hotmail.com>
2025-09-17 04:52:00 -0600

    Added axios package and configuration also added client configuration.

--- src/features/genres/components/GenresIndex.tsx
+++ src/features/genres/components/GenresIndex.tsx
@@ -1,8 +1,15 @@
 import { useNavigate } from "react-router";
 import Button from "../../../components/Button";
+import { useEffect } from "react";
+import apiClient from "../../../api/axiosClient";
 
 export default function GenresIndex() {
+
     const navigate = useNavigate();
+
+    useEffect(() => {
+        apiClient.get('/genres').then(res => console.log(res.data));
+    }, []);
 
     return (
         <>
