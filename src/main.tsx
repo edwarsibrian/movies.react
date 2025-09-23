a746d53cf15fd78b1038a8d0a021bcf090962eda
AccountsStatements <edward_el_doc@hotmail.com>
2025-09-23 02:14:03 -0600

    used get genres API endpoint TO Genres Index

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap-icons/font/bootstrap-icons.css'
 import 'leaflet/dist/leaflet.css'
 import App from './App.tsx'
 
